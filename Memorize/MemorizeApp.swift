7edb10cb1a9c43f3849438be54693e117d217b14
Alexander Shapiro <me@alexanderjshapiro.com>
2020-12-24 09:46:33 -0800

    Initialize project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexander Shapiro on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
